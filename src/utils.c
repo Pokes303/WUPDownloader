72b575d22adc9f52a53a5f9ef02a60f4df7523c1
Thomas Rohloff <v10lator@myway.de>
2020-04-01 11:06:31 +0200

    Even more pretty TV out.
    Signed-off-by: Thomas Rohloff <v10lator@myway.de>

--- src/utils.c
+++ src/utils.c
@@ -80,6 +80,16 @@ void paintLine(uint32_t column, uint32_t colour)
 	}
 }
 
+inline void writeDRC(uint32_t row, uint32_t column, const char* str)
+{
+	OSScreenPutFontEx(SCREEN_DRC, row, column, str);
+}
+
+inline void writeTV(uint32_t row, uint32_t column, const char* str)
+{
+	OSScreenPutFontEx(SCREEN_TV, row, column, str);
+}
+
 void write(uint32_t row, uint32_t column, const char* str) { //Write to the two screens
 	uint32_t dRow;
 	if(row > 64)
@@ -90,8 +100,8 @@ void write(uint32_t row, uint32_t column, const char* str) { //Write to the two 
 	}
 	else
 		dRow = row;
-	OSScreenPutFontEx(SCREEN_TV, row, column, str);
-	OSScreenPutFontEx(SCREEN_DRC, dRow, column, str);
+	writeTV(row, column, str);
+	writeDRC(dRow, column, str);
 }
 
 struct DownloadLogList;
@@ -101,64 +111,98 @@ struct DownloadLogList
 	char *line;
 	DownloadLogList *nextEntry;
 };
-DownloadLogList *downloadLogList = NULL;
+
+DownloadLogList *downloadLogList[2] = { NULL, NULL };
 void addToDownloadLog(char* str) {
 	WHBLogPrintf(str);
 	
-	DownloadLogList *newEntry = MEMAllocFromDefaultHeap(sizeof(DownloadLogList));
-	if(newEntry ==  NULL)
+	DownloadLogList *newTVEntry = MEMAllocFromDefaultHeap(sizeof(DownloadLogList));
+	if(newTVEntry ==  NULL)
 		return;
+	DownloadLogList *newDRCEntry = MEMAllocFromDefaultHeap(sizeof(DownloadLogList));
+	if(newDRCEntry ==  NULL)
+	{
+		MEMFreeToDefaultHeap(newTVEntry);
+		return;
+	}
 	
 	//TODO: We copy the string here for fast porting purposes
-	newEntry->line = MEMAllocFromDefaultHeap(sizeof(char) * (strlen(str) + 1));
-	if(newEntry->line == NULL)
+	newTVEntry->line = newDRCEntry->line = MEMAllocFromDefaultHeap(sizeof(char) * (strlen(str) + 1));
+	if(newTVEntry->line == NULL)
 	{
-		MEMFreeToDefaultHeap(newEntry);
+		MEMFreeToDefaultHeap(newTVEntry);
+		MEMFreeToDefaultHeap(newDRCEntry);
 		return;
 	}
-	strcpy(newEntry->line, str);
-	newEntry->nextEntry = NULL;
+	strcpy(newTVEntry->line, str);
 	
-	if(downloadLogList == NULL)
+	newTVEntry->nextEntry = newDRCEntry->nextEntry = NULL;
+	
+	if(downloadLogList[0] == NULL)
 	{
-		downloadLogList = newEntry;
+		downloadLogList[0] = newTVEntry;
+		downloadLogList[1] = newDRCEntry;
 		return;
 	}
 	
-	DownloadLogList *last;
+	DownloadLogList *lastTV;
 	int i = 0;
-	for(DownloadLogList *c = downloadLogList; c != NULL; c = c->nextEntry)
+	for(DownloadLogList *c = downloadLogList[0]; c != NULL; c = c->nextEntry)
 	{
 		
 		i++;
-		last = c;
+		lastTV = c;
 	}
-	
-	if(i == MAX_LINE_DRC)
+	if(i == MAX_LINE_TV)
 	{
-		DownloadLogList *tmpList = downloadLogList;
-		downloadLogList = tmpList->nextEntry;
+		DownloadLogList *tmpList = downloadLogList[0];
+		downloadLogList[0] = tmpList->nextEntry;
 		MEMFreeToDefaultHeap(tmpList->line);
 		MEMFreeToDefaultHeap(tmpList);
 	}
 	
-	last->nextEntry = newEntry;
+	DownloadLogList *lastDRC;
+	i = 0;
+	for(DownloadLogList *c = downloadLogList[1]; c != NULL; c = c->nextEntry)
+	{
+		
+		i++;
+		lastDRC = c;
+	}
+	if(i == MAX_LINE_DRC)
+	{
+		DownloadLogList *tmpList = downloadLogList[1];
+		downloadLogList[1] = tmpList->nextEntry;
+		MEMFreeToDefaultHeap(tmpList);
+	}
+	
+	lastTV->nextEntry = newTVEntry;
+	lastDRC->nextEntry = newDRCEntry;
 }
 void clearDownloadLog() {
 	DownloadLogList *tmpList;
-	while(downloadLogList != NULL)
+	while(downloadLogList[0] != NULL)
 	{
-		tmpList = downloadLogList;
-		downloadLogList = tmpList->nextEntry;
+		tmpList = downloadLogList[0];
+		downloadLogList[0] = tmpList->nextEntry;
 		MEMFreeToDefaultHeap(tmpList->line);
+		MEMFreeToDefaultHeap(tmpList);
+	}
+	while(downloadLogList[1] != NULL)
+	{
+		tmpList = downloadLogList[1];
+		downloadLogList[1] = tmpList->nextEntry;
 		MEMFreeToDefaultHeap(tmpList);
 	}
 }
 void writeDownloadLog() {
 	paintLine(2, SCREEN_COLOR_WHITE);
 	int i = 3;
-	for(DownloadLogList *entry = downloadLogList; entry != NULL; entry = entry->nextEntry)
-		write(0, i++, entry->line);
+	for(DownloadLogList *entry = downloadLogList[0]; entry != NULL; entry = entry->nextEntry)
+		writeTV(0, i++, entry->line);
+	i = 3;
+	for(DownloadLogList *entry = downloadLogList[1]; entry != NULL; entry = entry->nextEntry)
+		writeDRC(0, i++, entry->line);
 }
 
 void colorStartRefresh(uint32_t color) {
