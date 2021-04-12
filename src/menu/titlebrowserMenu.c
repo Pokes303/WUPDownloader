3e4c3ec2e0cbe1764eb12c1b0bb70adb7294023e
Thomas Rohloff <v10lator@myway.de>
2021-04-12 10:08:59 +0200

    Titlebrowser: Reset cursor position after searching.
    Signed-off-by: Thomas Rohloff <v10lator@myway.de>

--- src/menu/titlebrowserMenu.c
+++ src/menu/titlebrowserMenu.c
@@ -272,6 +272,7 @@ void titleBrowserMenu()
 		if(vpad.trigger & VPAD_BUTTON_Y)
 		{
 			showKeyboard(KEYBOARD_TYPE_NORMAL, search, CHECK_NONE, 128, false, search, "Search");
+			cursor = pos = 0;
 			redraw = true;
 		}
 		
