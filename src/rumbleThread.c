c9396e00c437abd6f17648c8bd48f032d5437a54
Thomas Rohloff <v10lator@myway.de>
2021-04-18 09:45:48 +0200

    Revert "Give rumble thead a higher stack size."
    This reverts commit 3dc507e68ded5296c40ac4e5d646f03a544ccd5a.

--- src/rumbleThread.c
+++ src/rumbleThread.c
@@ -28,7 +28,7 @@
 #include <osdefs.h>
 #include <utils.h>
 
-#define RUMBLE_STACK_SIZE 0x2000
+#define RUMBLE_STACK_SIZE 0x200
 
 static OSThread rumbleThread;
 static uint8_t rumbleThreadStack[RUMBLE_STACK_SIZE];
