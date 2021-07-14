777080fdf0ed6442125c0a6816b0daa266926379
Thomas Rohloff <v10lator@myway.de>
2021-07-14 10:23:35 +0200

    Don't log before the logger is initialized.
    Signed-off-by: Thomas Rohloff <v10lator@myway.de>

--- src/status.c
+++ src/status.c
@@ -130,10 +130,7 @@ void initStatus()
 #endif
 	
 	if(IMIsAPDEnabledBySysSettings((uint32_t *)&apdEnabled) != 0)
-	{
-		debugPrintf("Couldn't read APD sys setting!");
 		apdEnabled = false;
-	}
 	
 }
 
