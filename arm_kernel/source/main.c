1f1d79073ee4368378a2c2a958e135333c6d235e
Thomas Rohloff <v10lator@myway.de>
2021-12-02 07:52:38 +0100

    Partly revert 036f83181c506cde89e0a5bbdda54aeaea3d8eae
    Signed-off-by: Thomas Rohloff <v10lator@myway.de>

--- arm_kernel/source/main.c
+++ arm_kernel/source/main.c
@@ -55,9 +55,11 @@ int _main()
 	int(*disable_interrupts)() = (int(*)())0x0812E778;
 	int(*enable_interrupts)(int) = (int(*)(int))0x0812E78C;
 	void(*invalidate_dcache)(unsigned int, unsigned int) = (void(*)())0x08120164;
+	void(*flush_dcache)(unsigned int, unsigned int) = (void(*)())0x08120160;
 	char* (*kernel_memcpy)(void*, void*, int) = (char*(*)(void*, void*, int))0x08131D04;
 	int (*read_otp)(int, void *, unsigned int) = (int (*)(int, void *, unsigned int))0x08120248;
 
+	flush_dcache(0x081200F0, 0x4001); // giving a size >= 0x4000 flushes all cache
 	int level = disable_interrupts();
 	unsigned int control_register = disable_mmu();
 
@@ -83,7 +85,7 @@ int _main()
 
 	/* REENABLE MMU */
 	restore_mmu(control_register);
-	invalidate_dcache(0x081298BC, sizeof(repairData_set_fault_behavior));
+	invalidate_dcache(0x081298BC, 0x4001); // giving a size >= 0x4000 invalidates all cache
 	enable_interrupts(level);
 
     /* Finally copy the OTP */
