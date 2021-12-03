41ee70c5e4764661751d1ba354eba1c162073520
Thomas Rohloff <v10lator@myway.de>
2021-12-03 17:43:11 +0100

    Sanity checker: Check more files.
    Signed-off-by: Thomas Rohloff <v10lator@myway.de>

--- src/sanity.c
+++ src/sanity.c
@@ -36,8 +36,25 @@
 #include <coreinit/memdefaultheap.h>
 #include <coreinit/title.h>
 
+#define MD5_FILES 6
 
-static const uint8_t drcTexMd5[16] = { 0x33, 0xe1, 0xd6, 0x7f, 0x62, 0xc9, 0xf4, 0x48, 0xc5, 0xc0, 0x95, 0xe5, 0x95, 0xaa, 0x6f, 0xc6 };
+static const uint8_t md5[MD5_FILES][16] = {
+	{ 0x88, 0xbd, 0xe1, 0xac, 0xdd, 0xf1, 0x9e, 0x08, 0xfb, 0x1a, 0xe9, 0xf6, 0x6f, 0x97, 0x03, 0xe7 },
+	{ 0x33, 0xe1, 0xd6, 0x7f, 0x62, 0xc9, 0xf4, 0x48, 0xc5, 0xc0, 0x95, 0xe5, 0x95, 0xaa, 0x6f, 0xc6 },
+	{ 0xa7, 0xac, 0xd1, 0x88, 0x3e, 0x81, 0x6f, 0xab, 0x90, 0xff, 0x7e, 0x21, 0xa2, 0xae, 0xbb, 0xd2 },
+	{ 0x70, 0x56, 0xac, 0x62, 0x63, 0x45, 0x6a, 0x2e, 0x1d, 0x2b, 0x99, 0x2a, 0xee, 0x82, 0x22, 0x71 },
+	{ 0xc8, 0x85, 0xa1, 0x65, 0x3d, 0x16, 0x71, 0xb0, 0xaa, 0xd3, 0xf4, 0xf6, 0xeb, 0xdd, 0xab, 0x38 },
+	{ 0x0b, 0x77, 0xc7, 0x96, 0x6d, 0x1a, 0xbb, 0xc3, 0xc9, 0x15, 0x1a, 0xe4, 0xfa, 0x4a, 0x6d, 0x53 },
+};
+
+static const char *md5File[MD5_FILES] = {
+	"iconTex.tga",
+	"bootDrcTex.tga",
+	"BootTvTex.tga",
+	"bootLogoTex.tga",
+	"bootMovie.h264",
+	"bootSound.btsnd",
+};
 
 bool sanityCheck()
 {
@@ -69,47 +86,68 @@ bool sanityCheck()
 		}
 
 		char newPath[64];
-		bool ret = false;
+		FILE *f;
+		uint32_t s;
+		void *buf;
+		MD5_CTX md5c;
+		uint8_t m[16];
+		bool ret = true;
+		bool br = false;
+
 		if(isUsb)
 		{
-			strcpy(newPath, "usb:");
-			strcat(newPath, title.path + 18);
-			strcat(newPath, "/meta/bootDrcTex.tga");
 			mountUSB();
+			strcpy(newPath, "usb:");
 		}
+		else
+			return true; // TODO
 
-		if(fileExists(newPath))
+		strcat(newPath, title.path + 18);
+		strcat(newPath, "/meta/");
+
+		for(int i = 0; !br && i < MD5_FILES; i++)
 		{
-			FILE *f = fopen(newPath, "rb+");
-			if(f != NULL)
+			strcpy(newPath + 38, md5File[i]);
+			f = fopen(newPath, "rb+");
+			if(f == NULL)
 			{
-				uint32_t s = getFilesize(f);
-				void *buf = MEMAllocFromDefaultHeap(s);
-				if(buf != NULL)
-				{
-					if(fread(buf, s, 1, f) == 1)
-					{
-						MD5_CTX md5c;
-						uint8_t m[16];
-						MD5_Init(&md5c);
-						MD5_Update(&md5c, buf, s);
-						MD5_Final(&m[0], &md5c);
-
-						ret = true;
-						for(int i = 0; i < 16; i++)
-						{
-							if(m[i] != drcTexMd5[i])
-							{
-								debugPrintf("Sanity error: Data integrity error");
-								ret = false;
-								break;
-							}
-						}
-					}
-					MEMFreeToDefaultHeap(buf);
-				}
-				fclose(f);
+				ret = false;
+				break;
 			}
+
+			s = getFilesize(f);
+			buf = MEMAllocFromDefaultHeap(s);
+			if(buf == NULL)
+			{
+				ret = false;
+				break;
+			}
+
+			if(fread(buf, s, 1, f) != 1)
+			{
+				MEMFreeToDefaultHeap(buf);
+				fclose(f);
+				ret = false;
+				break;
+			}
+
+			MD5_Init(&md5c);
+			MD5_Update(&md5c, buf, s);
+			MD5_Final(&m[0], &md5c);
+
+			for(int j = 0; j < 16; j++)
+			{
+				if(m[j] != md5[i][j])
+				{
+					debugPrintf("Sanity error: Data integrity error");
+					ret = false;
+					br = true;
+					break;
+				}
+			}
+
+			MEMFreeToDefaultHeap(buf);
+			fclose(f);
 		}
 
 		if(isUsb)
