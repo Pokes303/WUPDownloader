594a5e9b185e47c69381a9446c174f9bb247f49f
Thomas Rohloff <v10lator@myway.de>
2022-01-10 09:18:33 +0100

    Rearrange main menu.
    Signed-off-by: Thomas Rohloff <v10lator@myway.de>

--- src/menu/mainMenu.c
+++ src/menu/mainMenu.c
@@ -50,11 +50,11 @@ void drawMainMenuFrame()
 	
 	textToFrame(4, ALIGNED_CENTER, NUSSPLI_COPYRIGHT);
 	
-	textToFrame(13, 0, "Press " BUTTON_A " to download content");
-	textToFrame(14, 0, "Press " BUTTON_X " to install content");
-	textToFrame(15, 0, "Press " BUTTON_Y " to generate a fake <title.tik> file");
-	textToFrame(16, 0, "Press " BUTTON_LEFT " for options");
-	textToFrame(17, 0, "Press " BUTTON_HOME " or " BUTTON_B " to exit");
+	textToFrame(11, 0, "Press " BUTTON_A " to download content");
+	textToFrame(12, 0, "Press " BUTTON_X " to install content");
+	textToFrame(13, 0, "Press " BUTTON_Y " to generate a fake <title.tik> file");
+	textToFrame(14, 0, "Press " BUTTON_LEFT " for options");
+	textToFrame(15, 0, "Press " BUTTON_HOME " or " BUTTON_B " to exit");
 	
 	textToFrame(7, MAX_CHARS - 27, "Thanks to:");
 	textToFrame(8, MAX_CHARS - 26, "• cJSON");
