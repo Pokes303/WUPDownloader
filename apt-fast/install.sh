4a8f470c293ea87ad9a3780d6171078019b3b91d
Thomas Rohloff <v10lator@myway.de>
2022-08-27 17:47:40 +0200

    install.sh: Don't check for root rights.
    Signed-off-by: Thomas Rohloff <v10lator@myway.de>

--- apt-fast/install.sh
+++ apt-fast/install.sh
@@ -1,24 +1,12 @@
 #!/bin/bash
 set -e
 
-apt_fast_installation() {
-  sudo apt-get update
+sudo apt-get update
 
-  if ! type aria2c >/dev/null 2>&1; then
-    sudo apt-get install -y aria2
-  fi
-
-  wget https://raw.githubusercontent.com/ilikenwf/apt-fast/master/apt-fast -O /usr/local/sbin/apt-fast
-  chmod +x /usr/local/sbin/apt-fast
-  wget https://raw.githubusercontent.com/V10lator/NUSspli/master/apt-fast/apt-fast.conf -O /etc/apt-fast.conf
-}
-
-
-if [[ "$EUID" -eq 0 ]]; then
-  apt_fast_installation
-else
-  type sudo >/dev/null 2>&1 || { echo "sudo not installed, change into root context" >&2; exit 1; }
-
-  DECL="$(declare -f apt_fast_installation)"
-  sudo bash -c "$DECL; apt_fast_installation"
+if ! type aria2c >/dev/null 2>&1; then
+  sudo apt-get install -y aria2
 fi
+
+wget https://raw.githubusercontent.com/ilikenwf/apt-fast/master/apt-fast -O /usr/local/sbin/apt-fast
+chmod +x /usr/local/sbin/apt-fast
+wget https://raw.githubusercontent.com/V10lator/NUSspli/master/apt-fast/apt-fast.conf -O /etc/apt-fast.conf
