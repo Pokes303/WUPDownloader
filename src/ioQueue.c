2b09f6d317b411427e4a0e23b81fcf641e763581
Thomas Rohloff <v10lator@myway.de>
2022-09-10 23:24:24 +0200

    I/O queue: Be more agressive.
    Signed-off-by: Thomas Rohloff <v10lator@myway.de>

--- src/ioQueue.c
+++ src/ioQueue.c
@@ -70,7 +70,7 @@ static int ioThreadMain(int argc, const char **argv)
     {
         if(entry->file == NULL)
         {
-            OSSleepTicks(OSMillisecondsToTicks(20));
+            OSSleepTicks(OSMillisecondsToTicks(2));
             continue;
         }
 
