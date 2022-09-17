2e272f792d92c80543b03c62849d30c67418f80a
Thomas Rohloff <v10lator@myway.de>
2022-09-17 13:46:03 +0200

    Remove another empty line.
    Signed-off-by: Thomas Rohloff <v10lator@myway.de>

--- src/menu/filebrowserMenu.c
+++ src/menu/filebrowserMenu.c
@@ -126,7 +126,6 @@ refreshDirList:
         while(FSReadDir(__wut_devoptab_fs_client, getCmdBlk(), dir, &entry, FS_ERROR_FLAG_ALL) == FS_STATUS_OK)
             if(entry.info.flags & FS_STAT_DIRECTORY) // Check if it's a directory
             {
-
                 len = strlen(entry.name);
                 name = MEMAllocFromDefaultHeap(len + 2);
                 if(name == NULL)
