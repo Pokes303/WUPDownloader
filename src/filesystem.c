07cce72d17d07423f3e844571fcb83896a4d54c6
Thomas Rohloff <v10lator@myway.de>
2023-07-06 13:46:43 +0200

    Fix typo.
    Signed-off-by: Thomas Rohloff <v10lator@myway.de>

--- src/filesystem.c
+++ src/filesystem.c
@@ -52,7 +52,7 @@ static int spaceThreadMain(int argc, const char **argv)
 
 void initFSSpace()
 {
-    spaceThread = startThread("NUSspli FS Initialiszer", THREAD_PRIORITY_MEDIUM, 0x400, spaceThreadMain, 0, NULL, OS_THREAD_ATTRIB_AFFINITY_ANY);
+    spaceThread = startThread("NUSspli FS Initialiser", THREAD_PRIORITY_MEDIUM, 0x400, spaceThreadMain, 0, NULL, OS_THREAD_ATTRIB_AFFINITY_ANY);
 }
 
 bool initFS(bool validCfw)
