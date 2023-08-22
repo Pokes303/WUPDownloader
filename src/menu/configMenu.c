48e56a171df63dd645d827d98ef13a6448402bba
Thomas Rohloff <v10lator@myway.de>
2023-08-22 15:24:11 +0200

    Wire in italian language file.
    Signed-off-by: Thomas Rohloff <v10lator@myway.de>

--- src/menu/configMenu.c
+++ src/menu/configMenu.c
@@ -90,6 +90,9 @@ static inline void switchMenuLanguage()
         switch(lang)
         {
             case Swkbd_LanguageType__Invalid:
+                lang = Swkbd_LanguageType__Italian;
+                break;
+            case Swkbd_LanguageType__Italian:
                 lang = Swkbd_LanguageType__French;
                 break;
             case Swkbd_LanguageType__French:
@@ -133,6 +136,9 @@ static inline void switchMenuLanguage()
                 break;
             case Swkbd_LanguageType__Portuguese_BR:
                 lang = Swkbd_LanguageType__French;
+                break;
+            case Swkbd_LanguageType__French:
+                lang = Swkbd_LanguageType__Italian;
                 break;
             default:
                 lang = Swkbd_LanguageType__Invalid;
